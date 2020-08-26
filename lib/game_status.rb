865f7ada464f0fd4ba9b2b7dc18125dbf37c46c6
Freda Hon <freda.hon@gmail.com>
2020-08-26 22:11:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,7 @@ def won?(board)
     
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_1 == "0" && position_2 == "0" && position_3 == "0"
       return win_combination
     else 
       return false
