97e72c7c64aef9128582e55d5c48e06751e67eba
Freda Hon <freda.hon@gmail.com>
2020-08-26 20:44:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,16 +18,16 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
-    win_index_1 = win_combination[0] # grab each index from each win_combination [child array]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+    win_index_1 = win_combo[0] # grab each index from each win_combination [child array]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
     
     position_1 = board[win_index_1] # load the value onto the board
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      return win_combo
     else 
       return false
     end
