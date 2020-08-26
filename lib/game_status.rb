69ce14a2095952970c6d512e382a4116f758344a
Freda Hon <freda.hon@gmail.com>
2020-08-26 20:42:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0] # grab each index from each child array
+    win_index_1 = win_combination[0] # grab each index from each win_combination [child array]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
     
@@ -58,4 +58,7 @@ def over?(board)
     return true
   end
 end
+
+def winner(board)
+  
     
