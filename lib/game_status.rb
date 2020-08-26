32e331a825cc7b0eabdbe6124a15b24d50d37973
Freda Hon <freda.hon@gmail.com>
2020-08-26 21:49:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
+  WIN_COMBINATIONS.each do |win_combo|
     index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
@@ -30,6 +30,6 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
-  }
+  end
   return false
 end
